b80ee7e1a66ac18940ab8df10308d93aaf9cdc33
MsManish09 <msmanish680@gmail.com>
2025-08-18 00:56:30 +0530

    user city search logic done. use location and recently viewed city list logic pending

--- app.js
+++ app.js
@@ -2,60 +2,121 @@
 
 // my api key, without which can't access the api
 const API_KEY = 'cb77ef33b9e8ab659ebeef8529bc7e8b'
-let CITY_NAME = 'Bengaluru'
+let CITY_NAME = 'Bengaluru' // Bengaluru would be the default city.
 
-// current weather api
-const  current_weather_api = `https://api.openweathermap.org/data/2.5/weather?q=${CITY_NAME}&appid=${API_KEY}&units=metric`
-
-// api provides weather data of next 5 days
-const api_5day_forecast = `https://api.openweathermap.org/data/2.5/forecast?q=${CITY_NAME}&appid=${API_KEY}&units=metric
-`
 
 // storing weather icon address a object for dynamic use
 let weather_icon= {
     Sun:{
         highlight: "images%20and%20resources/sunny_highlight.png",
-        normal: 'images%20and%20resources/sunny.png',
+        normal: 'images%20and%20resources/sunny.png'
     },
+    
     Clouds: {
-        normal:'images%20and%20resources/cloudy 2.png',
-        cloudy_highlight: 'images%20and%20resources/cloudy64px.png',
+        normal:'images%20and%20resources/cloudy.png',
+        highlight:'images%20and%20resources/cloudy64px.png'
     },
+
     Rain: {
         normal:  'images%20and%20resources/rainy-day.png',
         highlight: 'images%20and%20resources/rainy-high.png'
-    }
+    },
+    Thunderstrom:{
+        normal: 'images%20and%20resources/thunderstorm.png',
+        highlight: 'images%20and%20resources/thunderstorm.png'
+    }, 
+    Mist:{
+        normal:'images%20and%20resources/mist.png',
+        highlight:'images%20and%20resources/mist.png'
+    },
+    Smoke:{
+        normal:'images%20and%20resources/Smoke.png',
+        highlight:'images%20and%20resources/Smoke.png'
+    },
+    Haze:{
+        normal:'images%20and%20resources/haze.png',
+        highlight:'images%20and%20resources/haze.png'
+    },
 }
 
-// fetching current weather details
+// standardizing main weather description for icon matching
+function standard_main_weather(weather){
+    if (weather === 'Clear'){
+        return 'Sun'
+    }
+
+    if(weather === 'Drizzle'){
+        return 'Rain'
+    }
+    return weather
+}
+
+// functions to fetch data for apis and update weather.
+
+// fetching current weather details -------------
 async function fetch_current_weather_details() {
-    let repsonse = await fetch(current_weather_api)
-    let data = await repsonse.json()
-    console.log('current weather forecast')
+
+    // current weather api
+    const current_weather_api = `https://api.openweathermap.org/data/2.5/weather?q=${CITY_NAME}&appid=${API_KEY}&units=metric`
+
+    let response;
+    let data
+    try{
+        response = await fetch(current_weather_api)
+
+        // if fetch fails
+        if(!response.ok){
+            throw new Error('Error: Server did not respond')
+        }
+
+        data = await response.json()
+
+        // if city does not exist 
+        if(data.cod !== 200){
+            throw new Error(data.message)
+        }
+    }
+
+    catch(error){
+        let message_block = document.querySelector('#app_message')
+        message_block.classList.remove('hidden')
+        let message = document.querySelector('#message')
+        message.textContent = error
+
+        // show the error message(if city name is invalid) or fetch fail and reload after 3sec
+        async function reload_page() {
+            await new Promise(resolve =>{
+                setTimeout(() => {
+                window.location.reload()
+            }, 3000)
+            })
+        }
+        reload_page()
+    }
+    
     console.log(data)
 
-    let main_weather = data.weather[0].main
-    // if main  = 'Clear' change it to Sun, make easy to select icon
-    if(main_weather == 'Clear'){
-        main_weather = 'Sun'
-    }
-
-    if(main_weather == "Drizzle" || main_weather == "Thunderstorm"){
-        main_weather = 'Rain'
-    }
+    let main_weather = standard_main_weather(data.weather[0].main)
+    console.log(main_weather)
 
     // selecting elements to implemtn the data.
-
-    // selecting city name ele and applying city name
+    // selecting city name ele and updating city name
     let location_name = document.querySelector('#city_location')
-    // console.log(location_name)
     location_name.textContent = `${data.name}, ${data.sys.country}`
 
-    // selecting highlight temp ele and applying temp
+    // selecting highlight temp ele and updating temp
     let city_temp = document.querySelector('#current_temp')
     city_temp.textContent = `${data.main.temp}`
     let city_weather_icon = document.querySelector('#today_weather_desc_logo')
-    city_weather_icon.setAttribute('src', weather_icon[main_weather]['normal'] )
+    // city_weather_icon.setAttribute('src', weather_icon[main_weather]['normal'] )
+    if (weather_icon[main_weather]) {
+        city_weather_icon.setAttribute('src', weather_icon[main_weather]['normal']);
+    } 
+
+    // fallback, if main weather icon is not present.
+    else {
+        five_day_desc_icon[i].setAttribute('src', weather_icon.Mist.normal);
+    }
 
     // selecting main current temp ele and feel like temp ele
     let current_temp = document.querySelector('#curr_temp_ele')
@@ -64,13 +125,18 @@ async function fetch_current_weather_details() {
     let feel_like_temp = document.querySelector('#feels_like_temp')
     feel_like_temp.textContent = `${data.main.feels_like}`
 
-
     // selecting current weather desc ele
     let weather_desc_image = document.querySelector('#weather_desc_icon')
 
-    // selectig img ele and changing base on current weather
-
-    weather_desc_image.setAttribute('src', weather_icon[main_weather]['highlight']);
+    // selectig img ele and changing weather icon base on current weather
+    // weather_desc_image.setAttribute('src', weather_icon[main_weather]['highlight']);
+    if (weather_icon[main_weather]) {
+        weather_desc_image.setAttribute('src', weather_icon[main_weather]['highlight']);
+    } 
+    // fallback, if main weather icon is not present.
+    else {
+        five_day_desc_icon[i].setAttribute('src', weather_icon.Mist.normal);
+    }
 
     let weather_desc_ele = document.querySelector('#weather_desc_text')
     let text = data.weather[0].description
@@ -92,43 +158,37 @@ async function fetch_current_weather_details() {
     cloud_cover_ele.textContent = data.clouds.all
 }
 
-fetch_current_weather_details()
 
-
-// fetching 5day forecast data
+// fetching 5day forecast data ----------------------------
 async function fetch_5day_forecast() {
-    let repsonse = await fetch(api_5day_forecast)
-    let data = await repsonse.json()
-    console.log(data)
+
+    // api provides weather data of next 5 days
+    const api_5day_forecast = `https://api.openweathermap.org/data/2.5/forecast?q=${CITY_NAME}&appid=${API_KEY}&units=metric`
+
+    const response = await fetch(api_5day_forecast)
+    const data = await response.json()
+    // console.log(data)
 
     // selecting array of next 6 hours elements
     let first_block = document.querySelector('#first_block')
     let second_block = document.querySelector('#second_block')
     let third_block = document.querySelector ('#third_block') 
     let blocks = [first_block, second_block, third_block]
-    // console.log(blocks)
 
     // selecting temp ele of forecast3 forecast with 6 hours gap) 
     let temp_eles = document.querySelectorAll('.temp')
-    // console.log(temp_eles)
-
 
     // selecting weather desc ele and img ele of forecast3 forecast with 6 hours gap) 
     let weather_desc = document.querySelectorAll('.weather_desc')
-    // console.log(weather_desc)
 
-    // img ele
+    // weather icon ele
     let weather_desc_icon = document.querySelectorAll('.forecast_weather_icon')
-    // console.log(weather_desc_icon)
 
     // selecting rain ele of forecast3 forecast with 6 hours gap) 
     let rain_eles = document.querySelectorAll('.rain')
-    // console.log(rain_eles)
 
     // selecting rain ele of forecast3 forecast with 6 hours gap) 
     let wind_eles = document.querySelectorAll('.wind')
-    // console.log(wind_eles)
- 
 
     // adding date and time of forecast(3 forecast with 6 hours gap)
 
@@ -143,26 +203,27 @@ async function fetch_5day_forecast() {
         weather_desc[i].textContent = `${data.list[count].weather[0].description}`
         
         // finding the main weather
-        let main_weather = data.list[count].weather[0].main
-        if (main_weather === 'Clear') {
-            main_weather = 'Sun';
+        let main_weather = standard_main_weather(data.list[count].weather[0].main)
+        // weather_desc_icon[i].setAttribute('src', weather_icon[main_weather].normal )
+        if (weather_icon[main_weather]) {
+            weather_desc_icon[i].setAttribute('src', weather_icon[main_weather]['normal']);
+        } 
+        // fallback, if main weather icon is not present.
+        else {
+            weather_desc_icon[i].setAttribute('src', weather_icon.Mist.normal);
         }
-        if (main_weather === 'Drizzle' || main_weather === 'Thunderstorm') {
-            main_weather = 'Rain';
-        }
-        weather_desc_icon[i].setAttribute('src', weather_icon[main_weather]['normal'] )
 
         rain_eles[i].textContent = `3h: ${data.list[count].rain?.['3h'] ?? '0 '} mm`
 
         wind_eles[i].textContent = `${data.list[count].wind.speed} km/hr`
 
-        count += 2
+        count += 2 // we want use 1,3,5th items in the list to get data of forecast 6hours apart
     }
 
-    // five day forecast blocks update
-    let fiveday_forecast_count = 1
+    // selecting five day forecast date and time blocks update
+    let fiveday_forecast_count = 6
     let forecast_time_date_ele = document.querySelectorAll('.FDFC_date_time_ele')
-    console.log(forecast_time_date_ele)
+    // console.log(forecast_time_date_ele)
 
     // select temperature elements 
     let five_day_fc_temp_ele = document.querySelectorAll('.five_day_temp')
@@ -178,6 +239,7 @@ async function fetch_5day_forecast() {
     // selecting wind ele
     let five_day_wind_ele = document.querySelectorAll('.five_day_wind')
 
+
     // updating 5day forecast elements
     for(let i = 0; i < 5; i++){
         // udpaing date time eles
@@ -192,26 +254,43 @@ async function fetch_5day_forecast() {
         five_day_fc_weather_desc_ele[i].textContent = data.list[fiveday_forecast_count].weather[0].description
 
         let weather_main = data.list[fiveday_forecast_count].weather[0].main
-        five_day_desc_icon[i].setAttribute('src', weather_icon[weather_main]['normal'])
-        if (weather_main === 'Clear') {
-            weather_main = 'Sun';
+        weather_main = standard_main_weather(weather_main)
+        // five_day_desc_icon[i].setAttribute('src', weather_icon[weather_main]['normal'])
+        if (weather_icon[weather_main]) {
+            five_day_desc_icon[i].setAttribute('src', weather_icon[weather_main]['normal']);
+        } 
+        // fallback, if main weather icon is not present.
+        else {
+            five_day_desc_icon[i].setAttribute('src', weather_icon.Mist.normal);
         }
-        if (weather_main === 'Drizzle' || weather_main === 'Thunderstorm') {
-            weather_main = 'Rain';
-        }
-        
         // updating rain ele
         five_day_rain_ele[i].textContent = `3h: ${data.list[fiveday_forecast_count].rain?.['3h']  ?? '0'} mm`
 
         // updating wind ele
         five_day_wind_ele[i].textContent = `${data.list[fiveday_forecast_count].wind.speed} km/hr`
 
-
         // updating count for new day forecast
-        fiveday_forecast_count += 6
+        fiveday_forecast_count += 8
     }
     
 }
+
+fetch_current_weather_details() 
 fetch_5day_forecast()
 
+// Event listner - getting city name form user
+let city_search_btn = document.querySelector('#search_city')
+city_search_btn.addEventListener('click', ()=>{
+    let city_input = document.querySelector('#city_name').value
+    // console.log(city_input)
 
+    if(!city_input){
+        alert('Enter city name')
+        return
+    }
+
+    CITY_NAME = city_input
+    fetch_current_weather_details()
+    fetch_5day_forecast()
+
+})
